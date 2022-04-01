bfeb0af3306876622cb618fb537d4a4a9abc7819
Lucas Ignacio Gonzalez <lucasignaciogonzalez1999@gmail.com>
2022-04-01 19:36:42 -0300

    Agregando cors config

--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const routes = require('./routes/index.routes')
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('./src/public'))
+app.use(cors())
 app.use(routes)
 
 app.listen(process.env.PORT, () => console.log(`Server started on port http://localhost:${process.env.PORT}`))
